7ca503fcf8301248d4be050b1aa7d64eccc6435b
Cristián Berroeta <cristianberroeta@gmail.com>
2021-10-12 03:30:06 -0300

    Format Login Form

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {Header} from './components/Header';
 import {Main} from './components/Main';
 import {User} from './store/models/User';
 
 function App() {
-    const user: User | null = {id: "123", name: "John Doe"};
-    // const user: User | null = null;
+    // const user: User | null = {id: "123", name: "John Doe"};
+    const [user, setUser] = useState<User | null>(null);
     
     return (
         <BrowserRouter>
--- app/src/components/LoginForm.tsx
+++ app/src/components/LoginForm.tsx
@@ -4,8 +4,8 @@ interface Props {}
 
 export const LoginForm: React.FC<Props> = () => {
     return <form className={styles.LoginForm}>
-        <input type="email" name="email" id="email" />
-        <input type="password" name="password" id="password" />
-        <button>Login</button>
+        <input type="email" name="email" id="email" placeholder="Enter your name" />
+        <input type="password" name="password" id="password" placeholder="And your password" />
+        <button>Register / Login</button>
     </form>;
 };
